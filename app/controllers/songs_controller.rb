14480205cf6b517c851efbcac2e214c6037fb8d1
Joe Ganobsik <jganobsik@gmail.com>
2019-03-21 21:54:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,35 @@
 class SongsController < ApplicationController
+  def index
+		@songs = Song.all
+	end
+
+	def show
+		@song = Song.find(params[:id])
+	end
+
+	def new
+		@song = song.new
+	end
+
+	def create
+	  @song = song.new(song_params)
+	  @song.save
+	  redirect_to song_path(@song)
+	end
+
+	def edit
+		@song = song.find(params[:id])
+	end
+
+	def update
+	  @song = song.find(params[:id])
+	  @song.update(song_params)
+	  redirect_to song_path(@song)
+	end
+
+	private
+
+		def song_params
+			params.require(:song).permit(:name, :bio)
+		end
 end
